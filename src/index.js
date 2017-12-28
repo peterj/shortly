a10121b8be8fdca4a13438f20fabb312200d71c2
Peter Jausovec <peter.jausovec@gmail.com>
2017-12-28 11:59:12 -0800

    Add Apollo

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+    ApolloClient,
+    ApolloProvider,
+    createNetworkInterface,
+} from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+    uri: 'https://api.graph.cool/simple/v1/cjbqvtox232ur01419pee2xbi',
+});
+
+const client = new ApolloClient({ networkInterface });
+const withApolloProvider = Comp => (
+    <ApolloProvider client={client}>{Comp}</ApolloProvider>
+);
+
+ReactDOM.render(withApolloProvider(<App />), document.getElementById('root'));
 registerServiceWorker();
